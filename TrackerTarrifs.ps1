3f7f449faec172be501789ebdb559138d097bb81
Andrew Palmer <ajpalmer79@gmail.com>
2023-02-04 12:28:55 +0000

    Update TrackerTarrifs.ps1

--- TrackerTarrifs.ps1
+++ TrackerTarrifs.ps1
@@ -1,4 +1,5 @@
 $Tarrifs = Invoke-RestMethod -Uri https://api.octopus.energy/v1/products/SILVER-FLEX-22-11-25
+$CurrentDateTime = Get-Date
 
 foreach ($DNO in '_A', '_B', '_C', '_D', '_E', '_F', '_G', '_H', '_J', '_K', '_L', '_M', '_N', '_P') {
 
@@ -24,10 +25,11 @@ foreach ($DNO in '_A', '_B', '_C', '_D', '_E', '_F', '_G', '_H', '_J', '_K', '_L
     $GTarrif = $Tarrifs.single_register_gas_tariffs.$($DNO).direct_debit_monthly
     
     [PSCustomObject]@{
-        code = $Region
-        estandard_unit_rate_inc_vat = $ETarrif.standard_unit_rate_inc_vat
-        estanding_charge_inc_vat = $ETarrif.standing_charge_inc_vat
-        gstandard_unit_rate_inc_vat = $GTarrif.standard_unit_rate_inc_vat
-        gstanding_charge_inc_vat = $GTarrif.standing_charge_inc_vat
+        region = $Region
+        date = $CurrentDateTime
+        electricity_standard_unit_rate_inc_vat = $ETarrif.standard_unit_rate_inc_vat
+        electricity_standing_charge_inc_vat = $ETarrif.standing_charge_inc_vat
+        gas_standard_unit_rate_inc_vat = $GTarrif.standard_unit_rate_inc_vat
+        gas_standing_charge_inc_vat = $GTarrif.standing_charge_inc_vat
     }
 }
